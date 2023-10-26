76ba6bc7843fda2f0ed40f252c345128cbbd7112
shivaKeka <shiva.t@keka.com>
2023-10-26 10:06:16 +0530

    commit

--- src/app/login-employee/login-employee.component.ts
+++ src/app/login-employee/login-employee.component.ts
@@ -1,40 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ServicesService } from '../services/services.service';
 import jwt_decode from 'jwt-decode';
 
 @Component({
-  selector: 'app-login-employee',
-  standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
-  templateUrl: './login-employee.component.html',
-  styleUrls: ['./login-employee.component.scss']
+    selector: 'app-login-employee',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule],
+    templateUrl: './login-employee.component.html',
+    styleUrls: ['./login-employee.component.scss'],
 })
 export class LoginEmployeeComponent {
+    loginForm!: FormGroup;
+    data: any;
 
-  loginForm!:FormGroup;
-  data:any;
+    constructor(private fb: FormBuilder, private service: ServicesService) {}
 
-  constructor(private fb:FormBuilder,private service:ServicesService){}
+    ngOnInit() {
+        this.loginForm = this.fb.group({
+            email: [''],
+            password: [''],
+        });
+    }
 
-  ngOnInit(){
-    this.loginForm = this.fb.group({
-      email:[''],
-      password:['']
-    })
-  }
+    onSubmit() {
+        this.service.getVerifyEmployee(this.loginForm.value).subscribe({
+            next: (v) => {
+                this.data = v;
+                const decodedToken = jwt_decode(this.data.token);
+                console.log('Decoded', decodedToken);
+            },
 
-  onSubmit(){
-    this.service.getVerifyEmployee(this.loginForm.value).subscribe((data)=>{
-      this.data = data;
-      const decodedToken = jwt_decode(this.data.token);
-      console.log(decodedToken);
-
-    },(error)=>{
-      console.log('Error',error);
-    });
-  }
-
+            error: (e) => {
+                console.log('Error', e);
+            },
+        });
+    }
 }

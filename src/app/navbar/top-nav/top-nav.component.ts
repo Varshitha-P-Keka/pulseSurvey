a9990755085da2beb8974b2e9c7c83ed37432c4b
shivaKeka <shiva.t@keka.com>
2023-10-26 10:12:02 +0530

    second commit

---
+++ src/app/navbar/top-nav/top-nav.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DatePipe } from '@angular/common';
+
+import { loggeduser } from 'src/app/modals/modal';
+
+
+@Component({
+  selector: 'app-top-nav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './top-nav.component.html',
+  styleUrls: ['./top-nav.component.scss']
+})
+export class TopNavComponent {
+  @Input() userDetails:loggeduser={name:'',emailaddress:'',EmployeeId:'',role:''};
+  today:any;
+
+  constructor(private date:DatePipe){}
+
+  ngOnInit(){
+    this.userDetails=JSON.parse(<string>localStorage.getItem('currentUser'));
+    this.today= this.date.transform(new Date(),'dd MMM YYYY');
+  }
+}
